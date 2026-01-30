0f09be06418918b27b37e0730ef5ed2a2f3c8abd
Asus <artemmavcun57@gmail.com>
2026-01-30 15:44:46 +0300

    Add validation in Application layer

--- Application/AnaliticsReports/Queries/GetAnalyticsReportWithPagination/GetAnalyticsReportWithPaginationQueryValidation.cs
+++ Application/AnaliticsReports/Queries/GetAnalyticsReportWithPagination/GetAnalyticsReportWithPaginationQueryValidation.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Application.AnaliticsReports.Queries.GetAnalyticsReportWithPagination;
 
-namespace Application.AnaliticsReports.Queries.GetAnalyticsReportWithPagination
+public class GetAnalyticsReportWithPaginationQueryValidation
 {
-    internal class GetAnalyticsReportWithPaginationQueryValidation
-    {
-    }
+
 }
--- Application/CoinPair/Queries/GetAllCoinPairQuery/GetAllCoinQueryValidation.cs
+++ Application/CoinPair/Queries/GetAllCoinPairQuery/GetAllCoinQueryValidation.cs
@@ -4,9 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application.CoinPair.Queries.GetAllCoinPairQuery
+namespace Application.CoinPair.Queries.GetAllCoinPairQuery;
+
+public class GetAllCoinQueryValidation
 {
-    internal class GetAllCoinQueryValidation
-    {
-    }
 }
