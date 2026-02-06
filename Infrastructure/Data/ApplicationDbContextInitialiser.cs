83c48f6b336d9d1680a82d145552397d20acb77e
Asus <artemmavcun57@gmail.com>
2026-02-06 22:56:02 +0300

    Add database initialiser

---
+++ Infrastructure/Data/ApplicationDbContextInitialiser.cs
@@ -0,0 +1,94 @@
+﻿using Infrastructure.Services.Identity;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Domain.Constants;
+using Domain.Entities;
+
+namespace Infrastructure.Data;
+
+public static class InitialiserExtensions
+{
+    public static async Task InitialiseDatabaseAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateAsyncScope();
+        var initialiser = scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitialiser>();
+
+        await initialiser.InitialiseAsync();
+        await initialiser.SeedAsync();
+    }
+}
+
+
+public class ApplicationDbContextInitialiser
+{
+    private readonly ILogger<ApplicationDbContextInitialiser> _logger;
+    private readonly ApplicationDbContext _context;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public ApplicationDbContextInitialiser(ILogger<ApplicationDbContextInitialiser> logger, ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+    {
+        _logger = logger;
+        _context = context;
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+    public async Task InitialiseAsync()
+    {
+        try
+        {
+            await _context.Database.MigrateAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while initialising database.");
+            throw;
+        }
+    }
+
+    public async Task SeedAsync()
+    {
+        try
+        {
+            await TrySeedAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while seeding the database.");
+            throw;
+        }
+    }
+
+    public async Task TrySeedAsync()
+    {
+        var admin = new IdentityRole(Roles.Administrator);
+        if(_roleManager.Roles.All(r => r.Name != admin.Name))
+        {
+            await _roleManager.CreateAsync(admin);
+        }
+
+        var administrator = new ApplicationUser { UserName = "", Email = "" };
+        if(_userManager.Users.All(u => u.UserName != administrator.UserName))
+        {
+            await _userManager.CreateAsync(administrator, "");
+            if (!string.IsNullOrWhiteSpace(admin.Name)) 
+            { 
+                await _userManager.AddToRolesAsync(administrator, new[] { admin.Name }); 
+            }
+        }
+
+        if(!_context.CoinPairs.Any())
+        {
+            _context.CoinPairs.AddRange(
+                    new CoinPair("BTC/USDT"),
+                    new CoinPair("ETH/USDT"),
+                    new CoinPair("SOL/USDT")
+            );
+            await _context.SaveChangesAsync();
+        }
+    }
+}
